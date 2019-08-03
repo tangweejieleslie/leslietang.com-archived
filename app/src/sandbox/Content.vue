6775b833c24b75053dc9e29cc75c9e386ee9ad90
Leslie Tang <tangweejieleslie+github@gmail.com>
2019-08-03 13:06:35 +0800

    multiple layout patterns

--- app/src/sandbox/Content.vue
+++ app/src/sandbox/Content.vue
@@ -1,42 +1,50 @@
 <template>
 
-<v-container class="fill-height" >
-    <v-layout col wrap class="green mx-1 fill-height" >
-        
-        <v-flex sm6 class="pa-1">
-            <div class="blue lighten-2 pa-1 fill-height">
-                <h1>Hello 1</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                    Maiores consectetur, illo aliquam nemo a ab illum aliquid quidem 
-                    consequatur, doloremque rem beatae soluta iste esse.
-                    Modi tenetur accusamus harum pariatur?
-                </p>
-            </div>
+<v-container class="fill-height">
+
+    <v-layout col wrap class="purple">
+        <v-flex>
+            <v-layout col wrap class="green mx-1 fill-height" sm12 >
+                <v-flex sm6 class="pa-1" v-for="item in sample" v-bind:key="item">
+                    <div class="blue lighten-2 pa-1 fill-height">
+                        <h1> 
+                            {{ item.title }}
+                        </h1>
+                        <p>
+                            {{ item.text }}
+                        </p>
+                    </div>
+                </v-flex>
+                    
+            </v-layout>
         </v-flex>
-        <v-flex sm6 class="pa-1">
-            <div class="blue lighten-2 pa-1 fill-height">
-                <h1>Hello 1</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                    Maiores consectetur, illo aliquam nemo a ab illum aliquid quidem 
-                    consequatur, doloremque rem beatae soluta iste esse.
-                    Modi tenetur accusamus harum pariatur?
-                </p>
-            </div>
+
+        <v-flex>
+            <v-layout col wrap class="blue mx-1 fill-height" >
+                <v-layout row v-for="item in sample" v-bind:key="item">
+                    <v-flex sm2>
+
+                    </v-flex>
+                        <v-flex sm8 class="pa-1">
+                            <div class="blue lighten-2 pa-1 fill-height">
+                                <h1> 
+                                    {{ item.title }}
+                                </h1>
+                                <p>
+                                    {{ item.text }}
+                                </p>
+                            </div>
+                        </v-flex>            
+                    <v-flex sm2>
+
+                    </v-flex>
+                </v-layout>
+            </v-layout>               
         </v-flex>
-        <v-flex sm6 class="pa-1">
-            <div class="blue lighten-2 pa-1 fill-height">
-                <h1>Hello 1</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                    Maiores consectetur, illo aliquam nemo a ab illum aliquid quidem 
-                    consequatur, doloremque rem beatae soluta iste esse.
-                    Modi tenetur accusamus harum pariatur?
-                </p>
-            </div>
-        </v-flex>                
-    </v-layout>
+ 
+        </v-layout>
+
+
 </v-container>
 
 </template>
@@ -46,6 +54,32 @@ export default {
   name: "Content",
   components: {},
   data: () => ({
+      sample: [
+          {
+              "title": "Hello World",
+              "text": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores consectetur, illo aliquam nemo a ab illum aliquid quidem consequatur, doloremque rem beatae soluta iste esse. Modi tenetur accusamus harum pariatur?"
+          },
+          {
+              "title": "Hello World",
+              "text": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores consectetur, illo aliquam nemo a ab illum aliquid quidem consequatur, doloremque rem beatae soluta iste esse. Modi tenetur accusamus harum pariatur?"
+          },
+          {
+              "title": "Hello World",
+              "text": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores consectetur, illo aliquam nemo a ab illum aliquid quidem consequatur, doloremque rem beatae soluta iste esse. Modi tenetur accusamus harum pariatur?"
+          },
+          {
+              "title": "Hello World",
+              "text": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores consectetur, illo aliquam nemo a ab illum aliquid quidem consequatur, doloremque rem beatae soluta iste esse. Modi tenetur accusamus harum pariatur?"
+          },
+          {
+              "title": "Hello World",
+              "text": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores consectetur, illo aliquam nemo a ab illum aliquid quidem consequatur, doloremque rem beatae soluta iste esse. Modi tenetur accusamus harum pariatur?"
+          },
+          {
+              "title": "Hello World",
+              "text": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores consectetur, illo aliquam nemo a ab illum aliquid quidem consequatur, doloremque rem beatae soluta iste esse. Modi tenetur accusamus harum pariatur?"
+          }
+      ]
   })
 };
 </script>
